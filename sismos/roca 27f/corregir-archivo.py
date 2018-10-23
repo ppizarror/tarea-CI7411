cb287a4571a4223cf4a2fa743ca48d92fe3de70d
ppizarror <pablo.pizarro@ing.uchile.cl>
2018-10-23 16:28:46 -0300

    Corrige archivo inicial

---
+++ sismos/roca 27f/corregir-archivo.py
@@ -0,0 +1,24 @@
+# Carga el archivo
+from __future__ import print_function
+import sys
+
+if (len(sys.argv) < 1):
+    print('El archivo no ha sido pasado por argumento, llamado: python correctada.py <filedata>')
+filename = sys.argv[1]
+
+# Carga el archivo a una lista
+data = []
+f = open(filename)
+for i in f:
+    data.append(i.strip())
+f.close()  # Cierra el archivo
+
+# Guarda el archivo sin el strip
+u = open(filename, 'w')
+for i in data:
+    i = i.replace('  ', ' ')
+    i = i.replace(' ', '\t')
+    u.write(i+'\n')
+
+# Cierra el archivo
+u.close()
